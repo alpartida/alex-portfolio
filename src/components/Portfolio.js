94c5cd4068c62dc7dc3a597010c2d20fc50b91f4
Alex Partida <alexgpartida@gmail.com>
2021-10-12 21:04:57 -0700

    modified resume pdf. modified weather app link.

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -6,7 +6,7 @@ function Portfolio() {
         <h1>Alex's Portfolio</h1>
         <h2>Check out my latest work</h2>
         <p>A fully functional weather application. Enter your zip code and discover the weather in your area.</p>
-        <a href="https://zen-wiles-8e239b.netlify.app/" target="_blank"> Weather App</a>
+        <a href="https://epic-jackson-ede428.netlify.app/" target="_blank"> Weather App</a>
         <p>Love Dogs? Well, who doesn't. Type in your favorite breed and be delighted to see great pictures of different amazing doggos and pups.</p>
         <a href="https://pensive-gates-015856.netlify.app/" target="_blank"> Dog API</a>
         </>
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Document, Page } from 'react-pdf';
-import alexResume from '../assets/alexResume.PDF';
+import AlexPartidaResume2021 from '../assets/AlexPartidaResume2021.pdf';
 
 function Resume() {
     return (
         <div>
             <h1>Alexandra Partida's Resume </h1>
             <h2> Read more about my past experience and my transition into tech.</h2>
-                <a download href={alexResume}>
-            <button>
+            <a download href={AlexPartidaResume2021}>
+                <button>
                     Download Resume
             </button>
-                    </a>
-            <Document file={alexResume} options={{ workerSrc: "pdf.worker.js" }} >
+            </a>
+            <Document file={AlexPartidaResume2021} options={{ workerSrc: "pdf.worker.js" }} >
                 <Page pageNumber={1} />
             </Document>
         </div >
